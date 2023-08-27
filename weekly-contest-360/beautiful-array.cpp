e6dccb7e4a36e272c00bf9cdfef69918c61779c5
Anurag Kumar <125788570+anuragino@users.noreply.github.com>
2023-08-28 00:13:06 +0530

    Update beautiful-array.cpp

--- weekly-contest-360/beautiful-array.cpp
+++ weekly-contest-360/beautiful-array.cpp
@@ -1,3 +1,24 @@
+/*
+            2834. Find the Minimum Possible Sum of a Beautiful Array
+
+            
+    You are given positive integers n and target.
+
+    An array nums is beautiful if it meets the following conditions:
+    
+    nums.length == n.
+    nums consists of pairwise distinct positive integers.
+    There doesn't exist two distinct indices, i and j, in the range [0, n - 1], such that nums[i] + nums[j] == target.
+    Return the minimum possible sum that a beautiful array could have.
+
+    Input: n = 3, target = 3
+    Output: 8
+    Explanation: We can see that nums = [1,3,4] is beautiful.
+    - The array nums has length n = 3.
+    - The array nums consists of pairwise distinct positive integers.
+    - There doesn't exist two distinct indices, i and j, with nums[i] + nums[j] == 3.
+    It can be proven that 8 is the minimum possible sum that a beautiful array could have.
+*/
 class Solution {
 public:
     long long minimumPossibleSum(int n, int target) {
